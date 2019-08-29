b35c6434d3bbe592fca960ef9a40e5124e7a79a4
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-08-29 01:50:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   killers = ["The Nurse", "The Shape", "The Hillbilly", "The Cannibal", "arrays!"]
-  remove_element_from_end_of_array= killers.pop
+  array = killers.pop
   p killers
 end
 
