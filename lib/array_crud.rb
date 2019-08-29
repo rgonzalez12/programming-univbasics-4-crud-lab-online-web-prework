30a502437d272a2f834aed9f9b287ebe124d07c6
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-08-29 01:36:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  killers = []
+  killers = ["The Nurse", "The Shape", "The Hillbilly", "The Cannibal"]
 end
 
 def add_element_to_end_of_array(array, element)
