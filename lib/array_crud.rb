33257e6b7d2cbe8a4e455da8efea01562d720116
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-08-29 01:56:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,5 +48,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
+  killers = ["am", "wow", "The Nurse", "The Shape", "The Hillbilly", "The Cannibal", "arrays!"]
+  
 
 end
